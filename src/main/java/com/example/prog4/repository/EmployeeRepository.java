a817092a4e3e72b0fa1bde5274b17874f0d987f6
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-08-23 21:30:22 +0300

    feat: simple cnaps integration

---
+++ src/main/java/com/example/prog4/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.prog4.repository;
+
+import com.example.prog4.model.EmployeeFilter;
+import com.example.prog4.repository.entity.Employee;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+public interface EmployeeRepository {
+  Employee findById(String id);
+  List<Employee> findByCriteria(EmployeeFilter filter, Pageable pageable);
+  Employee save(Employee employee);
+}
